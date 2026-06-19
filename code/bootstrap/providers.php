048275f2b003db0acf3db9a02d5d15991d085a40
Puja13-dp <gainpuja529@gmail.com>
2026-06-19 14:55:15 +0600

    Update providers.php

--- code/bootstrap/providers.php
+++ code/bootstrap/providers.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use Laravel\Sanctum\SanctumServiceProvider;  // Example
 
 return [
     AppServiceProvider::class,
+    SanctumServiceProvider::class,  // ← Add if needed
 ];
